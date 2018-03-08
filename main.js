3c18be72885a2087a38ded78cbccf51577350063
Michael Chan <michael@innova-global.com>
2018-03-08 18:58:26 +0900

    Fix - completed and checked so no duplications | next: continue

--- main.js
+++ main.js
@@ -51,8 +51,14 @@ Apify.main(async () => {
         def_simple: $('#entryContent').last('div.di.entry-body__el').find('span.trans').eq(0).text().trim(),
     }
 
+    let totalBlocks = 0
+    $('div.di.entry-body__el').each((y) => {
+        totalBlocks = totalBlocks + 1
+    })
     // get meaning and examples
+    console.log('totalBlocks', totalBlocks)
     $('div.di.entry-body__el').each((i, e1) => {
+        console.log('div.di.entry-body__el', i)
         const definfogc = $(e1).find('div.di-head').find('span.di-info').find('span.gc').eq(0).text().trim()
 
         const grammar = $(e1).find('div.di-head').find('span.di-info').find('span.pos').eq(0).text().trim()
@@ -64,29 +70,35 @@ Apify.main(async () => {
         const form = definfogc !== 'feminine' && definfogc !== 'masculine' ? definfogc : ''
 
         let meaning = ''
+        let meaning_mono = ''
 
-        // tranverse each definition
-        $('div.sense-block').each((j, e2) => {
-            let examples = []
-            // get example phrases
-            $(e2).find('div.def-body div.examp.emphasized').each((k, e3) => {
-                examples.push({
-                    gender,
-                    level: LEVEL_TYPE.NOVOICE,
-                    mono: $(e3).find('span.eg').text().trim(),
-                    tran: $(e3).find('span.trans').text().trim()
+        // tranverse each definition, ignore the last defintion (def_simple, with no examples)
+        i == (totalBlocks - 1) ? null :
+            $(e1).find('div.sense-block').each((j, e2) => {
+                // console.log('div.sense-block', j)
+                let examples = []
+                // get example phrases
+                $(e2).find('div.def-body div.examp.emphasized').each((k, e3) => {
+                    // console.log('div.def-body', k)
+                    examples.push({
+                        gender,
+                        level: LEVEL_TYPE.NOVOICE,
+                        mono: $(e3).find('span.eg').text().trim(),
+                        tran: $(e3).find('span.trans').text().trim()
+                    })
+                })
+
+                // push each definition
+                meaning = $(e2).find('span.trans').eq(0).text().trim()
+                meaning_mono = $(e2).find('.def').eq(0).text().trim()
+                results.push({
+                    grammar,
+                    form,
+                    meaning,
+                    meaning_mono,
+                    examples
                 })
             })
-
-            // push each definition
-            meaning = $(e2).find('span.trans').eq(0).text().trim()
-            results.push({
-                grammar,
-                form,
-                meaning,
-                examples
-            })
-        })
         // console.log('result', result)
     })
 
